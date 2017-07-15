efa588f7ef1524a32fbf92ec0a665bb627cc3147
Miguel Paraz <mparaz@gmail.com>
2017-07-15 17:23:40 +1000

    Support adding to cart and getting totals.

--- ShoppingCart/index.js
+++ ShoppingCart/index.js
@@ -5,14 +5,33 @@ const Decimal = require('decimal.js');
 
 class ShoppingCart {
     constructor(pricingRules) {
+        const catalogue = {};
+
+        pricingRules.catalogue.forEach((catalogueItem) => {
+            catalogue[catalogueItem.productCode] = catalogueItem;
+        });
+
+        this._catalogue = catalogue;
+
+        this._items = [];
     }
 
     total() {
-        return new Decimal(0);
+        return this._items.reduce((sum, item) => {
+            return sum.add(item.price);
+        }, new Decimal(0));
     }
 
     items() {
-        return [];
+        // Prevent modification.
+        return this._items.slice();
+    }
+
+    add(item, promoCode) {
+        const catalogueItem = this._catalogue[item];
+        if (catalogueItem) {
+            this._items.push(catalogueItem);
+        }
     }
 }
 
--- test/cart.js
+++ test/cart.js
@@ -12,7 +12,9 @@ describe('The SIM Company Cart', () => {
         let cart;
 
         beforeEach(() => {
-            cart = new ShoppingCart({});
+            cart = new ShoppingCart({
+                catalogue: []
+            });
         });
 
         it('should exist', () => {
@@ -25,6 +27,99 @@ describe('The SIM Company Cart', () => {
 
         it('should have no items', () => {
             expect(cart.items()).to.deep.equal([]);
+        });
+    });
+
+    describe('An cart with a catalogue, no offers, and one item', () => {
+        let cart;
+
+        const item1 = {
+            productCode: 'productCode1',
+            productName: 'productName1',
+            price: Decimal(123.45)
+        };
+
+        beforeEach(() => {
+            cart = new ShoppingCart({
+                catalogue: [
+                    item1
+                ]
+            });
+            cart.add('productCode1');
+        });
+
+        it('should exist', () => {
+            expect(cart).to.be.an('object');
+        });
+
+        it('should have a total', () => {
+            expect(cart.total()).to.deep.equal(new Decimal(123.45));
+        });
+
+        it('should have an items', () => {
+            expect(cart.items()).to.deep.equal([item1]);
+        });
+    });
+
+    describe('An cart with no catalogue, no offers, and attempt to add an item', () => {
+        let cart;
+
+        beforeEach(() => {
+            cart = new ShoppingCart({
+                catalogue: []
+            });
+            cart.add('productCode1');
+        });
+
+        it('should exist', () => {
+            expect(cart).to.be.an('object');
+        });
+
+        it('should have no total', () => {
+            expect(cart.total()).to.deep.equal(new Decimal(0));
+        });
+
+        it('should have no items', () => {
+            expect(cart.items()).to.deep.equal([]);
+        });
+    });
+
+    describe('An cart with a catalogue, no offers, and two items', () => {
+        let cart;
+
+        const item1 = {
+            productCode: 'productCode1',
+            productName: 'productName1',
+            price: Decimal(123.45)
+        };
+
+        const item2 = {
+            productCode: 'productCode2',
+            productName: 'productName2',
+            price: Decimal(678.90)
+        };
+
+        beforeEach(() => {
+            cart = new ShoppingCart({
+                catalogue: [
+                    item1,
+                    item2
+                ]
+            });
+            cart.add('productCode1');
+            cart.add('productCode2');
+        });
+
+        it('should exist', () => {
+            expect(cart).to.be.an('object');
+        });
+
+        it('should have a total', () => {
+            expect(cart.total()).to.deep.equal(new Decimal(802.35));
+        });
+
+        it('should have an items', () => {
+            expect(cart.items()).to.deep.equal([item1, item2]);
         });
     });
 });
